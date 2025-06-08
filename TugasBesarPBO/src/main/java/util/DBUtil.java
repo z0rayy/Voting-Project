939ef098f2ed545a358c9a3fa44c90dab41a21b6
Rayhan Dwi Putra <ravrbae.dwi.p@gmail.com>
2025-06-08 23:06:48 +0700

    Update DBUtil.java

--- TugasBesarPBO/src/main/java/util/DBUtil.java
+++ TugasBesarPBO/src/main/java/util/DBUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/voting";
     private static final String USER = "root";
-    private static final String PASSWORD = "user123";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         try {
